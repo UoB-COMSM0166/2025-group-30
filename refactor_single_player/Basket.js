8e03a1a51d66b018a458c2bb21e857e73eb799e6
lingchen <138715540+lingchen2333@users.noreply.github.com>
2025-03-01 15:20:51 +0000

    changed basket and grass

--- refactor_single_player/Basket.js
+++ refactor_single_player/Basket.js
@@ -1,13 +1,21 @@
 class Basket {
-    constructor() {
-        this.x = 30;
+    constructor(isLeft) { //ifLeft is only applicable to pvp mode
+        this.isLeft = isLeft;
+
+        this.x = isLeft ? 10 : width - 60;
         this.y = height - 100;
+
         this.w = 100;
         this.h = 100;
+
+        this.active = true; // 默认篮子激活
     }
 
     show() {
-        fill(165, 42, 42);
-        rect(this.x, this.y, this.w, this.h);
+        // 只有在激活状态下才显示篮子
+        if (this.active) {
+            fill(165, 42, 42);
+            rect(this.x, this.y, this.w, this.h);
+        }
     }
 }
--- refactor_single_player/Grass.js
+++ refactor_single_player/Grass.js
@@ -1,10 +1,12 @@
 class Grass {
-    constructor(x, y) {
+    constructor(x, y, isLeft) {
         this.x = x;
         this.y = y;
         this.height = 40; //height
         this.width = 60; //width
         this.speed = 4;
+
+        this.isLeft = isLeft;
     }
     
     fall() {
@@ -12,7 +14,9 @@ class Grass {
     }
     
     show() {
+        stroke(0);
         fill(0, 255, 0);
         rect(this.x, this.y, this.width, this.height);
+        noStroke();
     }
 }
